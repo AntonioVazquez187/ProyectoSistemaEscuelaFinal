c1ccc10c4e7acee26bcee0cf070539ac90729ec3
Antonio Vazquez <AntonioVazquez0101@outlook.com>
2024-05-16 20:35:26 -0600

    Agregar archivos de proyecto.

---
+++ SistemaEscuelaFinal/ConexionVB.vb
@@ -0,0 +1,15 @@
+﻿Module ConexionVB
+    'La variable conexion es la que permite conectarnos a nuestro SQL
+    'data source= donde esta conectada nuestra base de datos
+    Public conn As New SqlClient.SqlConnection("data source= DESKTOP-5N1KP6P; initial catalog=SistemaEscuela2; integrated security= SSPI; persist security info= false; trusted_connection = yes;")
+    'Nos permite ejecutar los comandos que  tienen que ver con los procedimientos
+    'almacenados de sql
+    Public cmd As SqlClient.SqlCommand
+    'permite leer los datos
+    Public sqlread As SqlClient.SqlDataReader
+    Public Query As String
+    Public Rol As String
+    Public bandera = 0
+    Public Caso As Integer
+    Public paso As Integer = 0
+End Module
