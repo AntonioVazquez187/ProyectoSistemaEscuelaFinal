5df38893ac6cb1d10681a1d6c26e1ceef562b17d
Antonio Vazquez <AntonioVazquez0101@outlook.com>
2024-05-19 15:16:37 -0600

    cambio color fondo

--- SistemaEscuelaFinal/frmAlumnoGrupo.Designer.vb
+++ SistemaEscuelaFinal/frmAlumnoGrupo.Designer.vb
@@ -144,7 +144,7 @@ Partial Class frmAlumnoGrupo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.Fuchsia
+        Me.BackColor = System.Drawing.Color.BlueViolet
         Me.ClientSize = New System.Drawing.Size(457, 159)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.cmbGrupo)
