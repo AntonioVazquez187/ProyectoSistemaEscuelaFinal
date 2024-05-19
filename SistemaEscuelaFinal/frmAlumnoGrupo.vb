1f9d714c8ded8531b886f16559588592afe4628e
Antonio Vazquez <AntonioVazquez0101@outlook.com>
2024-05-18 22:15:39 -0600

    cambios en alumno grupo

--- SistemaEscuelaFinal/frmAlumnoGrupo.vb
+++ SistemaEscuelaFinal/frmAlumnoGrupo.vb
@@ -4,17 +4,21 @@
     End Sub
 
     Private Sub cmbAlumno_DropDown(sender As Object, e As EventArgs) Handles cmbAlumno.DropDown
-        conn.Open()
-        Query = "select Matrícula,Nombre from Alumno"
-        cmd = New SqlClient.SqlCommand(Query, conn)
-        cmd.ExecuteNonQuery()
-        sqlread = cmd.ExecuteReader
-        While sqlread.Read
-            cmbAlumno.Items.Add(New ObtenerID(sqlread("Nombre"), sqlread("Matrícula")))
-        End While
-        sqlread.Close()
-        cmd.Dispose()
-        conn.Close()
+        Try
+            conn.Open()
+            Query = "select Matrícula,Nombre from Alumno"
+            cmd = New SqlClient.SqlCommand(Query, conn)
+            cmd.ExecuteNonQuery()
+            sqlread = cmd.ExecuteReader
+            While sqlread.Read
+                cmbAlumno.Items.Add(New ObtenerID(sqlread("Nombre"), sqlread("Matrícula")))
+            End While
+            sqlread.Close()
+            cmd.Dispose()
+            conn.Close()
+        Catch ex As Exception
+            MsgBox("Error en la conexión." + ex.ToString)
+        End Try
     End Sub
 
     Private Sub limpiar()
@@ -23,19 +27,31 @@
     End Sub
 
     Private Sub cmbGrupo_DropDown(sender As Object, e As EventArgs) Handles cmbGrupo.DropDown
-        conn.Open()
-        Query = "select distinct g.semestre, c.nombre, g.turno, g.Id_carrera from grupo as g
+        Try
+            conn.Open()
+            Query = "select distinct g.semestre, c.nombre, g.turno, g.Id_carrera from grupo as g
                 JOIN carrera as c on g.Id_carrera=c.num_plan"
-        cmd = New SqlClient.SqlCommand(Query, conn)
-        cmd.ExecuteNonQuery()
-        sqlread = cmd.ExecuteReader
-        While sqlread.Read
-            Dim nombre As String = sqlread("semestre") & " - " & sqlread("nombre") & "-" & sqlread("turno").ToString()
-            Dim matricula As String = sqlread("Id_carrera").ToString
-            cmbGrupo.Items.Add(New ObtenerID(nombre, matricula))
-        End While
-        sqlread.Close()
-        cmd.Dispose()
-        conn.Close()
+            cmd = New SqlClient.SqlCommand(Query, conn)
+            cmd.ExecuteNonQuery()
+            sqlread = cmd.ExecuteReader
+            While sqlread.Read
+                Dim nombre As String = sqlread("semestre") & " - " & sqlread("nombre") & "-" & sqlread("turno").ToString()
+                Dim matricula As String = sqlread("Id_carrera").ToString
+                cmbGrupo.Items.Add(New ObtenerID(nombre, matricula))
+            End While
+            sqlread.Close()
+            cmd.Dispose()
+            conn.Close()
+        Catch ex As Exception
+            MsgBox("Error en la conexión." + ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub cmbAlumno_TextChanged(sender As Object, e As EventArgs) Handles cmbAlumno.TextChanged
+        Try
+
+        Catch ex As Exception
+            MsgBox("Error en la conexión." + ex.ToString)
+        End Try
     End Sub
 End Class
